c1d04cadbb7f452656cc8f77b17715bd7b5e51a6
geosmirnoff <geosmirnoff@yandex.ru>
2021-04-20 23:43:31 +0300

    Props has been added to Post component

--- src/components/Profile/Wall/Post/Post.js
+++ src/components/Profile/Wall/Post/Post.js
@@ -1,17 +1,17 @@
 import post from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <section className={post.item}>
-            <img src="https://i.pinimg.com/originals/a7/35/bd/a735bd89df1a0fb4c80ffa583585943e.jpg" className="avatar" alt="avatar" />
-            <p className={post.author}>Котокот</p>
-            <p className={post.date}>20.04.2021</p>
+            <img src={props.avatar} className="avatar" alt="avatar" />
+            <p className={post.author}>{props.author}</p>
+            <p className={post.date}>{props.date}</p>
             <article className={post.body}>
-                Мой человек - долбоящер <a href='#s'>Далее...</a>
+                {props.body} <a href='#s'>Далее...</a>
             </article>
             <div className={post.likes}>
                 <img src="https://toppng.com/uploads/preview/dog-paw-print-png-dog-paw-11563597387zsw1pcwdge.png" alt="paw" />
-                <span className={post.likes_number}>300</span>
+                <span className={post.likes_number}>{props.likes_number}</span>
             </div>
         </section>
     );
--- src/components/Profile/Wall/Wall.js
+++ src/components/Profile/Wall/Wall.js
@@ -12,9 +12,9 @@ const Wall = () => {
                 </form>
             </div>
             <div className="post-wall">
-                <Post />
-                <Post />
-                <Post />
+                <Post author="Котокот" date="20.04.2021" body="Мой человек - долбоящер" likes_number="300" avatar="https://i.pinimg.com/originals/a7/35/bd/a735bd89df1a0fb4c80ffa583585943e.jpg" />
+                <Post author="Котокот" date="19.04.2021" body="Давненько меня не гладили" likes_number="200" avatar="https://i.pinimg.com/originals/a7/35/bd/a735bd89df1a0fb4c80ffa583585943e.jpg" />
+                <Post author="Котокот" date="18.04.2021" body="100 дней без валерьянки" likes_number="100" avatar="https://i.pinimg.com/originals/a7/35/bd/a735bd89df1a0fb4c80ffa583585943e.jpg" />
             </div>
       </div>
     );
