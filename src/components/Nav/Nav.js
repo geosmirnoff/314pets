8bd4319e3f718d30a2e86b3b2a67506197f24141
geosmirnoff <geosmirnoff@yandex.ru>
2021-04-21 22:11:07 +0300

    NavItem subcomponent was added to Nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,19 @@
 import nav from './Nav.module.css';
+import NavItem from './NavItem';
 
 const Nav = () => {
     return (
-        <nav className="nav">
+        <nav className={nav.nav}>
             <ul className={nav.nav_list}>
                 {/* Применяем несколько классов с помощью ШАБЛОННЫХ СТРОК: */}
-                <li className={`${nav.item} ${nav.active}`}><a href="#s">Профиль</a></li>
+                {/* <li className={`${nav.item} ${nav.active}`}><a href="#s">Профиль</a></li>
                 <li className={nav.item}><a href="#s">Друзья</a></li>
                 <li className={nav.item}><a href="#s">Сообщения</a></li>
-                <li className={nav.item}><a href="#s">Фотографии</a></li>
+                <li className={nav.item}><a href="#s">Фотографии</a></li> */}
+                <NavItem item="Профиль" item_style="active" />
+                <NavItem item="Друзья" />
+                <NavItem item="Сообщения" />
+                <NavItem item="Фотографии" />
             </ul>
         </nav>
     );
---
+++ src/components/Nav/NavItem.js
@@ -0,0 +1,17 @@
+import nav_item from './NavItem.module.css';
+
+const NavItem = (props) => {
+
+    let item_style = nav_item.item;
+
+    if (props.item_style === "active") {
+
+        item_style = `${nav_item.item} ${nav_item.active}`;
+    }
+
+    return (
+        <li className={item_style}><a href="#s">{props.item}</a></li>
+    );
+}
+
+export default NavItem;
