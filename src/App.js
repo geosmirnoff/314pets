bafbda1254d7606ae63831271e12914c352b9e51
geosmirnoff <geosmirnoff@yandex.ru>
2021-04-21 23:22:00 +0300

    Development of Message component was started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import Profile from './components/Profile/Profile';
+// import Messages from './components/Messages/Messages';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Nav />
       <Profile />      
+      {/* <Messages /> */}
       <Footer />
     </div>
   );
---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,11 @@
+import msg from './Messages.module.css';
+
+const Messages = (props) => {
+    return (
+        <div>
+            <h3 className={msg.header}>Сообщения</h3>
+        </div>
+    );
+}
+
+export default Messages;
