6fc2129972dd26baf43ec138fe5ea5ce9b670e3a
geosmirnoff <geosmirnoff@yandex.ru>
2021-05-04 00:35:55 +0300

    Chats data and posts data have been moved to index.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Chats from './components/Chats/Chats';
 
-const App = () => {
+const App = (props) => {
+
+  // let chat_items_data = props.chat_items_data;
+  // let current_chat_data = props.current_chat_data;
+  // let companion_name = props.companion_name;
+  // let companion_img = props.companion_img;
+  // let posts_data = props.posts_data;
+
   return (
     <BrowserRouter>
       <div className="app-wrapper">
         <Header />
         <Nav />
         <div className="app-wrapper-main">
-          <Route component={Profile} path="/profile" />
-          <Route component={Chats} path="/chats" />
+          <Route path="/profile" 
+                 render={() => <Profile posts_data={props.posts_data} />} />
+          <Route path="/chats" 
+                 render={() => <Chats chat_items_data={props.chat_items_data} current_chat_data={props.current_chat_data}
+                                      companion_name={props.companion_name} companion_img={props.companion_img} />} />
+          {/* <Route component={Profile} path="/profile" />
+          <Route component={Chats} path="/chats" /> */}
           {/* <Profile /> */}
           {/* <Messages /> */}
         </div>
--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -4,102 +4,21 @@ import CurrentChat from './CurrentChat';
 
 const Chats = (props) => {
 
-    let chat_items_data = [
-        {
-            from_id: "id1",
-            from_img: "https://pbs.twimg.com/profile_images/938269312166563840/tbT6X3eU.jpg",
-            from_name: "Пёсопёс",
-            text: "Йо, петух, стрела за гаражами",
-            date: "30.04.21"
-        },
-        {
-            from_id: "id2",
-            from_img: "https://krasivosti.pro/uploads/posts/2021-04/1618486033_16-krasivosti_pro-p-riba-v-ochkakh-ribi-krasivo-foto-17.jpg",
-            from_name: "Рыборыб",
-            text: "Есть чо? А еси найду?",
-            date: "29.04.21"
-        },
-        {
-            from_id: "id3",
-            from_img: "https://wallbox.ru/resize/640x960/wallpapers/main/201546/20309cc1d731ed6.jpg",
-            from_name: "Крысокрыс",
-            text: "Го пить, я создал",
-            date: "28.04.21"
-        },
-    ];
-
-    let current_chat_data = [
-        {
-            companion_id: "id5",
-            message_id: "msg1",
-            text: "Дороу",
-            date: "03.05.2021",
-            time: "17:58",
-            direction: "to_me"
-        },
-        {
-            companion_id: "id5",
-            message_id: "msg2",
-            text: "Дратути",
-            date: "03.05.2021",
-            time: "17:59",
-            direction: "from_me"
-        },
-        {
-            companion_id: "id5",
-            message_id: "msg3",
-            text: "Как жись?",
-            date: "03.05.2021",
-            time: "18:05",
-            direction: "to_me"
-        },
-        {
-            companion_id: "id5",
-            message_id: "msg4",
-            text: "Намана",
-            date: "03.05.2021",
-            time: "18:06",
-            direction: "from_me"
-        },
-        {
-            companion_id: "id5",
-            message_id: "msg5",
-            text: "Што делоиш?",
-            date: "03.05.2021",
-            time: "18:06",
-            direction: "to_me"
-        },
-        {
-            companion_id: "id5",
-            message_id: "msg6",
-            text: "Кушою",
-            date: "03.05.2021",
-            time: "18:07",
-            direction: "from_me"
-        },
-        {
-            companion_id: "id5",
-            message_id: "msg7",
-            text: "Панятна",
-            date: "03.05.2021",
-            time: "18:14",
-            direction: "to_me"
-        }
-    ];
-
-    let companion_name = "Птицептиц";
-    let companion_img = "https://www.meme-arsenal.com/memes/67e204168b032eea544ad1a45f880945.jpg";
+    // let chat_items_data = props.chat_items_data;
+    // let current_chat_data = props.current_chat_data;
+    // let companion_name = props.companion_name;
+    // let companion_img = props.companion_img;
 
     return (
         <div className={chats.wrapper}>
             <div className={chats.list}>
                 {/* List of chats */}
-                <ChatList chat_items_data={chat_items_data} />
+                <ChatList chat_items_data={props.chat_items_data} />
             </div>
             <div className={chats.current}>
-                <CurrentChat companion_name={companion_name} 
-                             companion_img={companion_img}
-                             current_chat_data={current_chat_data} />
+                <CurrentChat companion_name={props.companion_name} 
+                             companion_img={props.companion_img}
+                             current_chat_data={props.current_chat_data} />
             </div>
         </div>
     );
